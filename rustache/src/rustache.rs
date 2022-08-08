9579bd93c7ee974ec60ced047168ca91183967e5
Mihail Tratsiuk <git@mtratsi.uk>
2022-08-08 22:06:39 +0200

    Add loop support

--- rustache/src/rustache.rs
+++ rustache/src/rustache.rs
@@ -1,15 +1,20 @@
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
-use std::iter::Peekable;
 use std::path::Path;
-use std::str::Chars;
+
+type TemplatePair = (u8, u8);
+type TemplateError = Box<dyn Error>;
 
 const TEMPLATE_NAME: &str = "index.html";
-const VARIABLE_OPEN: (u8, u8) = (b'{', b'{');
-const VARIABLE_CLOSE: (u8, u8) = (b'}', b'}');
+const LOOP_ITEM_VARIABLE: &str = "$it";
+const VARIABLE_OPEN: TemplatePair = (b'{', b'{');
+const VARIABLE_CLOSE: TemplatePair = (b'}', b'}');
+const LOOP_OPEN: TemplatePair = (b'{', b'*');
+const LOOP_CLOSE: TemplatePair = (b'*', b'}');
+const BLOCK_END: TemplatePair = (b'{', b'}');
 
-pub fn render(input: &Path, output: &Path) -> Result<(), Box<dyn Error>> {
+pub fn render(input: &Path, output: &Path) -> Result<(), TemplateError> {
     let mut parser = Parser::from(input);
     parser.run()?;
     let result = parser.result()?;
@@ -19,11 +24,18 @@ pub fn render(input: &Path, output: &Path) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[derive(Debug)]
+enum Variable {
+    Value(String),
+    Array(Vec<String>),
+}
+
+#[derive(Debug)]
 struct Parser {
     in_bytes: Vec<u8>,
     out_bytes: Vec<u8>,
     pos: usize,
-    scopes: Vec<HashMap<String, String>>,
+    scopes: Vec<HashMap<String, Variable>>,
 }
 
 impl Parser {
@@ -40,33 +52,55 @@ impl Parser {
             in_bytes,
             out_bytes,
             pos: 0,
-            scopes: vec![HashMap::from([("name".to_string(), "Misha".to_string())])],
+            scopes: vec![HashMap::from([
+                ("name".to_string(), Variable::Value("Misha".to_string())),
+                (
+                    "items".to_string(),
+                    Variable::Array(vec!["first!".to_string(), "second!".to_string()]),
+                ),
+            ])],
         }
     }
 
-    fn run(&mut self) -> Result<(), Box<dyn Error>> {
+    fn run(&mut self) -> Result<(), TemplateError> {
         while self.pos < self.in_bytes.len() {
-            self.html()?;
+            self.run_html()?;
         }
 
         Ok(())
     }
 
-    fn result(self) -> Result<String, Box<dyn Error>> {
+    fn result(self) -> Result<String, TemplateError> {
         Ok(String::from_utf8(self.out_bytes)?)
     }
 
-    fn html(&mut self) -> Result<(), Box<dyn Error>> {
+    fn run_html(&mut self) -> Result<(), TemplateError> {
         loop {
             match self.peek_pair() {
                 Some(pair) => match pair {
                     VARIABLE_OPEN => {
                         self.skip(2);
-                        return self.variable();
+                        self.run_variable()?;
                     }
-                    _ => self.consume(2),
+                    LOOP_OPEN => {
+                        self.skip(2);
+                        self.run_loop()?;
+                    }
+                    BLOCK_END => {
+                        if self.scopes.len() > 1 {
+                            // if we are inside the block scope,
+                            // stop and give control back to previous parser
+                            // it will take care of the closing characters
+                            break;
+                        } else {
+                            // skip otherwise
+                            self.consume(2);
+                        }
+                    }
+                    _ => self.consume(1),
                 },
                 None => {
+                    // Only single char left, consume and stop
                     self.consume(1);
                     break;
                 }
@@ -76,17 +110,56 @@ impl Parser {
         Ok(())
     }
 
-    fn variable(&mut self) -> Result<(), Box<dyn Error>> {
+    fn run_variable(&mut self) -> Result<(), TemplateError> {
         let name = self.skip_until_pair(VARIABLE_CLOSE)?;
-        let value = self.get_value(&name).ok_or(format!("Undefined variable {}", name))?;
+        self.skip(2);
+
+        let variable = self
+            .get_value(&name)
+            .ok_or(format!("Undefined variable {}", name))?;
+
+        let value = match variable {
+            Variable::Value(x) => x.clone(),
+            _ => return Err(format!("Expected {} to be variable", name))?,
+        };
 
         self.emit(&mut value.into_bytes());
+
+        Ok(())
+    }
+
+    fn run_loop(&mut self) -> Result<(), TemplateError> {
+        let name = self.skip_until_pair(LOOP_CLOSE)?;
+        self.skip(2);
+
+        let variable = self
+            .get_value(&name)
+            .ok_or(format!("Undefined variable {}", name))?;
+
+        let items = match variable {
+            Variable::Array(x) => x.clone(),
+            _ => return Err(format!("Expected {} to be array", name))?,
+        };
+
+        let return_pos = self.pos;
+
+        for item in items {
+            self.pos = return_pos;
+
+            let scope = HashMap::from([(LOOP_ITEM_VARIABLE.to_string(), Variable::Value(item))]);
+            self.scopes.push(scope);
+
+            self.run_html()?;
+
+            self.scopes.pop();
+        }
+
         self.skip(2);
 
         Ok(())
     }
 
-    fn peek_pair(&self) -> Option<(u8, u8)> {
+    fn peek_pair(&self) -> Option<TemplatePair> {
         return if self.pos + 2 > self.in_bytes.len() {
             None
         } else {
@@ -98,7 +171,7 @@ impl Parser {
         self.pos += n;
     }
 
-    fn skip_until_pair(&mut self, pair: (u8, u8)) -> Result<String, Box<dyn Error>> {
+    fn skip_until_pair(&mut self, pair: TemplatePair) -> Result<String, TemplateError> {
         let mut name = vec![];
 
         while self
@@ -127,25 +200,13 @@ impl Parser {
         self.out_bytes.append(bytes);
     }
 
-    fn get_value(&mut self, key: &str) -> Option<String> {
+    fn get_value(&mut self, key: &str) -> Option<&Variable> {
         for scope in self.scopes.iter().rev() {
             if let Some(value) = scope.get(key) {
-                return Some(value.clone())
+                return Some(value);
             }
         }
 
         None
     }
 }
-
-// enum TemplateNode {
-//     Text(&str),
-//     Variable(&str),
-//     Loop(&str, &str, Vec<TemplateNode>),
-// }
-
-// struct Parser2 {
-//   in_bytes: Vec<u8>,
-//   pos: usize,
-//   current_parser: fn(&mut Parser) -> Result<(), Box<dyn Error>>,
-// }
