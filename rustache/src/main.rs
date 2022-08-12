24a44e610a96c3a0d6b2f4ae49fd98db93943264
Mihail Tratsiuk <git@mtratsi.uk>
2022-08-12 14:36:09 +0200

    Add cli arguments

--- rustache/src/main.rs
+++ rustache/src/main.rs
@@ -1,14 +1,32 @@
-mod rustache;
 mod ron;
+mod rustache;
 
-use std::path::Path;
+use std::{env, path::Path};
 
 fn main() {
-    let input = Path::new("./src");
-    let output = Path::new("./index.html");
+    let args: Vec<String> = env::args().collect();
+
+    let input = read_path_arg(
+        &args,
+        "in",
+        "Expected input path argument e.g.: `--in=./src`",
+    );
+    let output = read_path_arg(
+        &args,
+        "out",
+        "Expected output path argument e.g.: `--out=./build/index.html`",
+    );
 
     match rustache::render(input, output) {
-      Ok(_) => println!("Done!"),
-      Err(error) => println!("Failed to rustache: {}", error)
+        Ok(_) => println!("Done!"),
+        Err(error) => println!("Failed to rustache: {error}"),
     }
+}
+
+fn read_path_arg<'a>(args: &'a Vec<String>, name: &'a str, err_msg: &'a str) -> &'a Path {
+    args.iter()
+        .find(|a| a.starts_with(&format!("--{name}")))
+        .and_then(|a| a.split('=').last())
+        .map(Path::new)
+        .expect(err_msg)
 }
