7b5fb4bcb4eb25a537224718cf7179a0987c0d89
Mihail Tratsiuk <git@mtratsi.uk>
2022-08-13 01:26:09 +0200

    Update page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-(() => {
-    function main() {
-        console.log("He11lo ")
-    }
+(function () {
+    var DARK_THEME_CLASSNAME = "dark";
+
+    var themeToggle = document.getElementById("theme-checkbox");
+
+    themeToggle.addEventListener('change', function () {
+        document.documentElement.classList.toggle(DARK_THEME_CLASSNAME);
+    });
 })();
