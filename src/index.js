f8d0440300941353e9c65cff18d4c2588ee4b64b
Misha Tratsiuk <misha+git@spris.dev>
2024-06-05 00:04:22 +0200

    Update page layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 (function () {
     var DARK_THEME_CLASSNAME = "dark";
+    var THEME_STORAGE_KEY = "b3-theme"
+    var THEMES = {
+        JEDI: "jedi",
+        SITH: "sith",
+    }
 
-    var themeToggle = document.getElementById("theme-checkbox");
+    var state = tryExec(function () {
+        return window.localStorage.getItem(THEME_STORAGE_KEY)
+    }, THEMES.JEDI)
 
-    themeToggle.addEventListener('change', function () {
-        document.documentElement.classList.toggle(DARK_THEME_CLASSNAME);
-    });
+    if (state === THEMES.SITH && !document.documentElement.classList.contains(DARK_THEME_CLASSNAME)) {
+        document.documentElement.classList.add(DARK_THEME_CLASSNAME);
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var themeToggle = document.getElementById("theme-toggle");
+
+        if (state === THEMES.SITH && themeToggle.textContent !== THEMES.SITH) {
+            themeToggle.textContent = THEMES.SITH
+        }
+
+        themeToggle.addEventListener("click", function () {
+            if (state === THEMES.JEDI) {
+                state = THEMES.SITH
+                document.documentElement.classList.add(DARK_THEME_CLASSNAME);
+            } else {
+                state = THEMES.JEDI
+                document.documentElement.classList.remove(DARK_THEME_CLASSNAME);
+            }
+
+            themeToggle.textContent = state
+            tryExec(function () { window.localStorage.setItem(THEME_STORAGE_KEY, state) })
+        });
+    })
+
+    function tryExec(fn, fallback) {
+        try {
+            return fn() || fallback
+        } catch (err) {
+            console.warn(err)
+            return fallback
+        }
+    }
 })();
